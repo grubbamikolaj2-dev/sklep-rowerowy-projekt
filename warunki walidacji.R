9410c4b797c1a5f15b80d6bf7ba7815bf7b97197
Piotr Czarnecki <s182142@student.pg.edu.pl>
2025-11-17 11:52:15 +0100

    up

--- warunki walidacji.R
+++ warunki walidacji.R
@@ -1,7 +1,14 @@
-
-cars, childresn, age - liczby całkowite, skokowe
-age > 0
-income >= 0
-cars >= 0
-purchased bike <yes, no>
-  
+###Warunki walidacji
+#### ilościowe
+cars, children, age - liczby całkowite, skokowe
+children, income, cars, age, commute_distance -> >= 0
+#### Jakościowe
+##### binarne
+home_owner - Yes, no
+purchased bike <Yes, No>
+martial_status <married, single>
+gender <male,female>
+#####
+education
+occupation
+region
