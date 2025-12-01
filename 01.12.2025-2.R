4a269a4d62b56aafafc2c953beb09f9d63b3cd11
grubbamikolaj2-dev <grubbamikolaj2@gmail.com>
2025-12-01 10:53:19 +0100

    częśtość dla jakościowych

---
+++ 01.12.2025-2.R
@@ -0,0 +1,44 @@
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(kableExtra)
+
+
+
+# 2. Wybierz zmienne jakościowe
+dane_cat <- dane2 %>% select(where(~ is.character(.x) || is.factor(.x)))
+
+# 3. Obliczenie częstości i procentów — wszystko w jednej pętli
+wynik <- data.frame()
+
+for (colname in names(dane_cat)) {
+  
+  # tabela częstości
+  t <- as.data.frame(table(dane_cat[[colname]], useNA = "ifany"))
+  colnames(t) <- c("kategoria", "liczba")
+  
+  # dodanie procentów
+  t$procent <- round(100 * t$liczba / sum(t$liczba), 2)
+  
+  # dodanie nazwy zmiennej
+  t$zmienna <- colname
+  
+  # łączenie wyników
+  wynik <- rbind(wynik, t)
+}
+
+# 4. Uporządkowanie kolumn
+wynik <- wynik %>% select(zmienna, kategoria, liczba, procent)
+
+# 5. Tabela kableExtra
+wynik %>%
+  kable("html",
+        caption = "Statystyki opisowe dla zmiennych jakościowych (częstości)",
+        digits = 2) %>%
+  kable_styling(
+    bootstrap_options = c("striped", "hover", "condensed"),
+    full_width = FALSE,
+    position = "center"
+  ) %>%
+  scroll_box(height = "500px")
