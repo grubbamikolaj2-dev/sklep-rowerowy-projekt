9e1c761fd758eb56e1ee19144d31cb22a23282fa
grubbamikolaj2-dev <grubbamikolaj2@gmail.com>
2026-01-25 18:56:38 +0100

    zmiany dot. opisu modelu

--- model logitowy.R
+++ model logitowy.R
@@ -9,7 +9,6 @@ df_clean <- df %>%
 
 # Sprawdzenie struktury danych przed modelowaniem
 str(df_clean)
-table(df_clean)
 # 3. Budowa Modelu Logitowego
 model <- glm(Purchased.Bike ~ marital_status+gender+income +children +home_owner + cars + age +Education + Occupation, data = df_clean, family = binomial(link = "logit"))
 
@@ -25,3 +24,34 @@ print(or)
 prob <- predict(model, type = "response")
 predicted_classes <- ifelse(prob > 0.5, "Yes", "No")
 table(Predicted = predicted_classes, Actual = df_clean$Purchased.Bike)
+
+
+# Wymagane pakiety
+library(broom)
+library(dplyr)
+library(knitr)
+library(kableExtra)
+
+# 1. Wyciągnięcie uporządkowanych danych z modelu
+
+wyniki_modelu <- tidy(model, conf.int = TRUE, exponentiate = TRUE)
+
+# 2. Filtrowanie i formatowanie tabeli
+tabela_istotne <- wyniki_modelu %>%
+  filter(p.value < 0.05) %>%
+  filter(term != "(Intercept)") %>%
+  mutate(
+    OR_Formatted = round(estimate, 2), # Zaokrąglenie OR
+    CI_Formatted = paste0("[", round(conf.low, 2), " - ", round(conf.high, 2), "]"), # Ładne przedziały ufności
+    p_value_Formatted = format.pval(p.value, digits = 3, eps = 0.001) # Formatowanie p-value (np. <0.001)
+  ) %>%
+  select(term, OR_Formatted, CI_Formatted, p_value_Formatted)
+
+# 3. Generowanie eleganckiej tabeli
+kable(tabela_istotne, 
+      col.names = c("Zmienna", "Iloraz Szans (OR)", "95% Przedział Ufności", "P-value"),
+      align = "lccc",
+      caption = "Istotne statystycznie determinanty zakupu roweru (Model Logitowy)") %>%
+  kable_styling(bootstrap_options = c("striped", "hover", "condensed"), full_width = F) %>%
+  column_spec(2, bold = TRUE, color = "darkblue") %>% # Wyróżnienie kolumny z OR
+  footnote(general = "OR > 1 oznacza wzrost szansy na zakup, OR < 1 oznacza spadek szansy.")
